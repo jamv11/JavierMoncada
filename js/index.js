1f55dee2c304596c713cc380fe9e401dfc8d5633
jamv11 <j.a.m.v11@outlook.com>
2021-05-04 13:56:24 -0400

    animaciones en elementos scrollReveal

---
+++ js/index.js
@@ -0,0 +1,53 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.tit-cont', { 
+        duration: 3000,
+        scale:0.93
+        /* origin: 'top', */
+        /* distance: '-10px' */
+    });
+    sr.reveal('.carta-perfil', { 
+        duration: 1000,
+        /* scale:1.2 */
+        origin: 'top',
+        distance: '-50px'
+    });
+    sr.reveal('.contenedor-barras', { 
+        duration: 1000,
+        /* scale:1.2 */
+        origin: 'top',
+        distance: '-50px'
+    });
+    sr.reveal('.card-proyecto-1', { 
+        duration: 1000,
+        scale:1.1
+        /* origin: 'top',
+        distance: '-50px' */
+    });
+    sr.reveal('.card-proyecto-2', { 
+        duration: 1000,
+        scale:1.1,
+        delay:500
+      /*   origin: 'top',
+        distance: '-50px' */
+    });
+    sr.reveal('.contenedor-contacto', { 
+        duration: 1000,
+        scale:1.1,
+      /*   origin: 'top',
+        distance: '-50px' */
+    });
+    sr.reveal('.curriculum', { 
+        duration: 1000,
+        /* scale:1.1, */
+        origin: 'top',
+        distance: '-50px'
+    });
+
+    
+
+
+
+    
+
+    
